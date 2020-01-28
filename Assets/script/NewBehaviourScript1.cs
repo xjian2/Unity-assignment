e126658194b163c43d9eea39bb9853a2c2660b8d
Xiuqi Jian <xjian@computerart.net>
2020-01-28 14:57:11 -0500

    added scripts

---
+++ Assets/script/NewBehaviourScript1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    public Vector3 position;
+    public Vector3 retation;
+    public Vector3 sacle;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Vector3 jiggle = new Vector3(
+            position.x,
+            pisition.y + Random.value,
+            position.z);
+        this.transform.position = jiggle;
+        this.transform.
+    }
+}
